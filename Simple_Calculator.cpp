fe1f2551a3499902607dc1502c3d8f6962764a51
Shifatpreet <shifatpreetsingh@gmail.com>
2025-07-21 22:14:29 +0530

    Add files via upload

---
+++ Simple_Calculator.cpp
@@ -0,0 +1,30 @@
+//SHIFATPREET SINGH
+//PRN :- 24070123098
+//ENTC B1
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b;
+    cout<<"::Calculator Program::"<<endl;
+    cout<<"Enter the first number :"<<endl;
+    cin>>a;
+    cout<<"Enter the second number:"<<endl;
+    cin>>b;
+    cout<<"SUM OF NUMBERS ARE:"<<a+b<<endl;
+    cout<<"SUBTRACTION OF NUMBERS ARE:"<<a-b<<endl;
+    cout<<"MULTIPLICATION OF NUMBERS ARE :"<<a*b<<endl;
+    cout<<"DIVISION OF NUMBERS ARE:"<<a/b<<endl;
+    return 0;
+}
+/*
+OUTPUT
+::Calculator Program::
+Enter the first number :
+9
+Enter the second number:
+8
+SUM OF NUMBERS ARE:17
+SUBTRACTION OF NUMBERS ARE:1
+MULTIPLICATION OF NUMBERS ARE :72
+DIVISION OF NUMBERS ARE:1.125
+*/
